32f67a0571b82ef28a90ddf9bc4235f39e436f6b
akazakov <andrey.kazakov@aplana.com>
2019-02-21 18:31:47 +0300

    update

--- src/main/java/chat/SimpleChatClientA.java
+++ src/main/java/chat/SimpleChatClientA.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -11,19 +12,33 @@ import java.net.Socket;
 public class SimpleChatClientA {
 
 JTextField outgoing;
+JTextArea incoming;
 PrintWriter writer;
+BufferedReader reader;
 Socket sock;
 
 public void go() {
     JFrame frame = new JFrame("Simple Chat Client");
     JPanel mainPanel = new JPanel();
     outgoing = new JTextField(20);
+    incoming = new JTextArea(15, 50);
+    incoming.setLineWrap(true);
+    incoming.setWrapStyleWord(true);
+    incoming.setEditable(false);
+    JScrollPane qScroller = new JScrollPane(incoming);
+    qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
     JButton sendButton = new JButton("Send");
     sendButton.addActionListener(new SendButtonListener());
+    mainPanel.add(qScroller);
     mainPanel.add(outgoing);
     mainPanel.add(sendButton);
     frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
     setUpNetworking();
+
+    Thread readerThread = new Thread(new IncomingReader());
+    readerThread.start();
+
     frame.setSize(400,500);
     frame.setVisible(true);
 }
